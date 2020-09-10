c0a2ea1012e26fc9fa9a4d98b18f4e39c3234c5a
Joseph Abraham <7526379+seppu@users.noreply.github.com>
2020-09-10 12:11:09 +1000

    Initial commit

---
+++ src/data/ardata.ts
@@ -0,0 +1,34 @@
+const ardata = [
+  {
+    scenario: "Current",
+    "Mobile Voice": "672349",
+    "Mobile VoiceColor": "#e8c1a0",
+    "Mobile Data": "991811",
+    "Mobile DataColor": "#f47560",
+    "Net Spend": "1626705",
+    "Net SpendColor": "#ffff",
+    "Revenue change": "0%"
+  },
+  {
+    scenario: "Proposed",
+    "Mobile Voice": "592364",
+    "Mobile VoiceColor": "#e8c1a0",
+    "Mobile Data": "829865",
+    "Mobile DataColor": "#f47560",
+    "Net Spend": "1280006",
+    "Net SpendColor": "#ffff",
+    "Revenue change": "-21%"
+  },
+  {
+    scenario: "Requested",
+    "Mobile Voice": "420000",
+    "Mobile VoiceColor": "#e8c1a0",
+    "Mobile Data": "790000",
+    "Mobile DataColor": "#f47560",
+    "Net Spend": "1010000",
+    "Net SpendColor": "#ffff",
+    "Revenue change": "-38%"
+  }
+];
+
+export default ardata;
